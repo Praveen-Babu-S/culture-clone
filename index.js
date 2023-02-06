1a79d8adab229d16e73d8ecf724a5a9820fa28f2
Praveen Babu <praveenbabu22793@gmail.com>
2023-02-06 21:15:00 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/* DAY-1 */
+// const a = 0
+// const b = false
+
+// if (a == b) {
+//     console.log("YES");
+// }
+// else {
+//     console.log("NO")
+// }
+
+// console.log(fun(3));
+
+// function fun(x) {
+//     if ((x >= 10 && x & 1 == 0) || (x < 10 && x & 1))
+//         return true;
+//     return false
+// }
+
+
+const var_name = () => {
+    //block of statements
+}
